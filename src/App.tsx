202bc23fe95648ac4a1263ea3e73d4cd57be4108
Anastasiya <n-asenchik@yandex.ru>
2022-04-14 09:24:18 +0300

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Routes from 'Constants/Routes';
 import Loading from './components/Loading';
 
 const Login = lazy(() => import('Pages/Login'));
-const Game = lazy(() => import('Pages/Game/Game'));
+const Game = lazy(() => import('Pages/Game'));
 const Forum = lazy(() => import('Pages/Forum'));
 const Profile = lazy(() => import('Pages/Profile'));
 const Rules = lazy(() => import('Pages/Rules'));
