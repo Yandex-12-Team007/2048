daad5901c551968b6bfd05f4d7279a2bfeca4b5a
Gavrik Alexandr <few0000@yandex.ru>
2022-05-19 14:39:26 +0300

    Drop webpack dev server

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,13 @@
 import path from 'path';
 import express from 'express';
-import webpack from 'webpack';
 import compression from 'compression';
 import serverRenderMiddleware from './server/middleware/serverRenderMiddleware';
-import webpackMiddleware from 'webpack-dev-middleware';
 import '@babel/polyfill';
-
-import webpackConfig from '../webpack/client.config';
-const compiler = webpack(webpackConfig);
 
 const app = express();
 
 app.use(compression())
     .use(express.static(path.resolve(__dirname, '../dist')));
-
-app.use(
-    webpackMiddleware(compiler, {
-      serverSideRender: true,
-    })
-);
 
 app.get('*', serverRenderMiddleware);
 
